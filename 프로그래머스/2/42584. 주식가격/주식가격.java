a70524ebd1277beb23ddc497d151707971884d6a
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-01-07 12:52:50 +0900

    [level 2] Title: 주식가격, Time: 19.42 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,45 +1,15 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
         
-        int priceLength = prices.length;
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        int[] answer = new int[priceLength];
-        
-        for(int i = 0; i < priceLength; i++){
-            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
-                int j = stack.pop();
-                
-                answer[j] = i - j;
-                
+        for(int i = 0; i < prices.length; i++){
+            for(int j = i + 1; j < prices.length; j++){
+                answer[i]++;
+                if(prices[i] > prices[j]){
+                    break;
+                }
             }
-            
-            stack.push(i);
-            
         }
-        
-        while(!stack.isEmpty()){
-            int p = stack.pop();
-            answer[p] = (priceLength - 1) - p;
-        }
-        
         return answer;
-        
-        
     }
 }
-
-/*
-
-[1, 2, 3, *2*, 3]
-- 떨어진 지점이 변곡점
-[1, 2, 3, *2*] 
-[-, -, 1, -]
-
-
-- index 저장
-- 새로운 게 들어오면, 전의 값들과 비교
-
-*/
