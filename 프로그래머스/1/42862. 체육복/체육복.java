83cd254e91f1f0046b0eeb3e9e65a4344e73c432
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-01-20 09:55:09 +0900

    [level 1] Title: 체육복, Time: 0.24 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,8 +2,6 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         
-        
-        // lost, reserve 중복 학생 지우기 위해서 Set 생성
         Set<Integer> lostSet = new HashSet<>();
         Set<Integer> reserveSet = new HashSet<>();
         
@@ -13,30 +11,55 @@ class Solution {
         for(int j: reserve){
             reserveSet.add(j);
         }
-        
-        // lost, reserve 모두에 있는 학생 지우기
-        Set<Integer> tempSet = new HashSet<>(lostSet);
-        
-        for(int t: tempSet){
-            if(reserveSet.contains(t)){
-                lostSet.remove(t);
-                reserveSet.remove(t);
+        // 1. 중복 제거
+        Set<Integer> tempSet = new HashSet<>(reserveSet);
+        for(Integer p: tempSet){
+            if(lostSet.contains(p)){
+                lostSet.remove(p);
+                reserveSet.remove(p);
             }
         }
         
+        // 2. List로 만들어서 정렬
+        // List<Integer> lostList = new ArrayList<>(lostSet);
         List<Integer> reserveList = new ArrayList<>(reserveSet);
+        
+        // Collections.sort(lostList);
         Collections.sort(reserveList);
         
-        for(int p: reserveList){
-            if(lostSet.contains(p - 1)){
-                lostSet.remove(p - 1);
+        // 3. 
+        
+        for(Integer q: reserveList){
+            if(lostSet.contains(q - 1)){
+                lostSet.remove(q - 1);
+                reserveSet.remove(q);
             }
-            else if(lostSet.contains(p + 1)){
-                lostSet.remove(p + 1);
+            else if(lostSet.contains(q + 1)){
+                lostSet.remove(q + 1);
+                reserveSet.remove(q);
             }
         }
         
         return n - lostSet.size();
+        
     }
 }
 
+
+
+// 1. lost, reserve 모두에 있는 학생 lost에서 제외
+// 2. reservce 학생 기준으로 앞 -> 뒤 순서로 빌려주기
+
+// -> return n - lost 수
+
+// ArrayList vs Set
+// - ArrayList
+//     - contains(3): 값 3 있나?
+//     - remove(3): 인덱스 3 삭제
+// - Set
+//     - contains(3): 값 3 있나?
+//     - remove(3): 값 3 삭제
+    
+// - ArrayList의 remove
+//     - remove(int index): 인덱스 삭제 -> remove(3)
+//     - remove(Object o): 값 삭제    -> remove(Integer.valueOf(3))
