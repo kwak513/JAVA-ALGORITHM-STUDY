12c40367e6b8635b6afd2c1c56e8edde03c70430
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-14 18:42:24 +0900

    [level 2] Title: 주식가격, Time: 31.86 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,34 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
+        
+        // 인덱스 넣을 스택
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        // 정답 담을 배열
         int[] answer = new int[prices.length];
         
+            
         for(int i = 0; i < prices.length; i++){
-            for(int j = i + 1; j < prices.length; j++){
-                answer[i]++;
-                if(prices[i] > prices[j]){
-                    break;
+            
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
+                    answer[stack.peek()] = i - stack.peek();
+                    stack.pop();
                 }
-            }
+                
+            stack.push(i);
+            
         }
+        
+        while(!stack.isEmpty()){
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
+            
+        }
+        
+        
         return answer;
+        
     }
 }
