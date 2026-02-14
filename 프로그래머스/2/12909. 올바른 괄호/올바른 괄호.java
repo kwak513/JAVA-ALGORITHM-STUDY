0c13ab21f0e36552029c1d6f7afa51b3a639b7ec
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-14 16:23:02 +0900

    [level 2] Title: 올바른 괄호, Time: 18.35 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,43 +1,26 @@
+import java.util.*;
 class Solution {
     boolean solution(String s) {
-        int openNum = 0;
         
-        // s 순회하면서 체크
-        for(int i =0; i < s.length(); i++){
-            char currentChar = s.charAt(i);
-            
-            if(currentChar == '('){
-                openNum++;
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stack.push(c);
             }
-            else if(currentChar == ')'){
-                // 조건 1 체크
-                if(openNum == 0){
+            else if(c == ')'){
+                if(stack.isEmpty()){
                     return false;
                 }
-                
-                openNum--;
-                
+                else{
+                    stack.pop();
+                }
             }
+            
+            
         }
         
-        // 조건 2 체크
-        if(openNum == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return stack.isEmpty();
         
     }
 }
-
-/*
-[언제 false?]
-- 조건 1. )가 들어오는 순간, 매칭되는 (가 없으면 바로 false
-- 조건 2. ()()( 처럼 괄호가 닫히지 않으면 false
-
-[헷갈린 문법]
-1. String.charAt(i): 반환은 char
-2. char는 == 비교
-
-*/
