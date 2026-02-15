2ed9638d47d7fb9548358127db646666023cf662
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-15 17:26:55 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,74 +3,48 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        int[] timeArray = new int[speeds.length];
+        // 작업 시간 저장할 큐
+        Queue<Integer> timeQueue = new ArrayDeque<>();
         
-        // 100% 되는데 걸리는 시간 계산
+        // 작업 시간을 큐에 저장
         for(int i = 0; i < progresses.length; i++){
-            // i번째의 progresses, speeds 추출
-            int currentProgress = progresses[i];
-            int currentSpeed = speeds[i];
+            int time = (100 - progresses[i]) % speeds[i] != 0 ? (100 - progresses[i]) / speeds[i] + 1 : (100 - progresses[i]) / speeds[i];
+            timeQueue.add(time);
             
-            int time = (100 - currentProgress) / currentSpeed;
-            int timeNamuji = (100 - currentProgress) % currentSpeed;
-            
-            if(timeNamuji == 0){
-                time = time;
-            }
-            else{
-                time = time + 1;
-            }
-            
-            timeArray[i] = time;    //  [2일, 1일, 3일]
         }
         
-        //  [2일, 1일, 3일] -> [2일, 2일, 3일]
-        for(int j = 0; j < timeArray.length - 1; j++){
-            int currentTime = timeArray[j];
-            int nextTime = timeArray[j + 1];
+        List<Integer> answerList = new ArrayList<>();
+        
+        // poll 하면서 대소 비교
+        while(!timeQueue.isEmpty()){
             
-            if(currentTime > nextTime){
-                timeArray[j + 1] = currentTime;
+            // return 할 count
+            int count = 0;
+            // poll한 값
+            int polledInt = timeQueue.poll();
+            count++;
+            
+            
+            // poll >= peek -> poll후, count++
+            // 함께 나갈 것들 '전부' 꺼내기
+            while(!timeQueue.isEmpty() && polledInt >= timeQueue.peek()){
+                timeQueue.poll();
+                count++;
             }
+            
+            answerList.add(count);
         }
         
-        //  [2일, 2일, 3일] -> {2일: 2, 3일: 1}
-                
-        Map<Integer, Integer> answerMap = new LinkedHashMap<>();
+        // answerList -> int[]
         
-        for(int p = 0; p < timeArray.length; p++){
-            int num = answerMap.getOrDefault(timeArray[p], 0);
-            answerMap.put(timeArray[p], num + 1);
-        }
-        
-        // value만 추출: [2, 1]
-        ArrayList<Integer> answerList =  new ArrayList<>(answerMap.values());
-        
-        // ArrayList -> int[]
         int[] answer = new int[answerList.size()];
         
-        for(int q = 0; q < answerList.size(); q++){
-            answer[q] = answerList.get(q);
+        for(int p = 0; p < answerList.size(); p++){
+            answer[p] = answerList.get(p);
+        
         }
         
         return answer;
         
-       
-        
-    
-        
-        
     }
 }
-
-
-/*
-[문제 풀이]
-ex) progress: 30, speed: 40 -> (100 - 30) / 40 = 1 -> 나머지가 있으면 + 1 -> 총 2일 
-    progress: 95, speed: 10 -> (100 - 95) / 10 = 0 -> 나머지가 있으면 + 1 -> 총 1일
-    
-    - [2일, 1일, 3일] -> 순회하면서 이전꺼보다 작으면 이전꺼로 바꿈 -> [2일, 2일, 3일] 
-    - 같은 날짜인 것의 개수를 배열로 return.
-
-[]
-*/
