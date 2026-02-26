6c4e8b23b9ee0cd8c8c226cebcfe721c6fdc0c95
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-26 16:12:48 +0900

    [level 1] Title: 폰켓몬, Time: 8.56 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,36 +1,27 @@
 import java.util.*;
 
-/*
-    ex) [3번, 1번, 2번, 3번] => 3번: 2개, 1번: 1개, 2번: 1개
-        - 2개 골라라: 종류 2개
-        ------------------------ 포켓몬 종류 ? 고를 개수
-        - 3개 골라라: 종류 3개
-        - 4개 골라라: 종류 3개
-        - 5개 골라라: 종류 3개
-
-*/
 class Solution {
     public int solution(int[] nums) {
         
-        HashSet<Integer> hashSet = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
         
         for(int num: nums){
-            hashSet.add(num);
+            int currentVal = map.getOrDefault(num, 0);
+            
+            map.put(num, currentVal);
         }
         
-        // 고를 포켓몬 개수
-        int numToPick = nums.length / 2;
+        int numToSelect = nums.length / 2;
+        int mapLength = map.keySet().size();
         
-        if(numToPick >= hashSet.size()){
-            return hashSet.size();
+        if(numToSelect >= mapLength){
+            return mapLength;
+        }
+        else{
+            return numToSelect;
         }
         
-        return numToPick;
+        
         
     }
 }
-
-/*
-    - HashSet<Integer>(): 문법 실수, () 제외
-    - HashMap을 사용할 필요가 없었음. (중요한 건 '어떤' 종류인지가 아니라, 종류의 '개수'이기 때문)
-*/
