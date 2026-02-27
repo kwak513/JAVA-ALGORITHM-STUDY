719a7e4eeda39c8d3cff61696f1bd628493a8d99
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-27 19:03:52 +0900

    [level 1] Title: 모의고사, Time: 0.80 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,59 +1,43 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int answerLength = answers.length;
         
-        int[] pattern1 = {1, 2, 3, 4, 5};
-        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] pattern1 = new int[]{1, 2, 3, 4, 5};
+        int[] pattern2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+        int[] pattern3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] answer1 = new int[answerLength];
-        int[] answer2 = new int[answerLength];
-        int[] answer3 = new int[answerLength];
+        int score1 = 0;
+        int score2 = 0;
+        int score3 = 0;
+        
+        for(int i = 0; i < answers.length; i++){
+            int currentAnswer = answers[i];
+            
+            int current1 = pattern1[i % pattern1.length];
+            int current2 = pattern2[i % pattern2.length];
+            int current3 = pattern3[i % pattern3.length];
+            
+            if(current1 == currentAnswer) score1++;
+            if(current2 == currentAnswer) score2++;
+            if(current3 == currentAnswer) score3++;
             
             
-        for(int i = 0; i < answerLength; i++){
-            answer1[i] = pattern1[i % 5];
-            answer2[i] = pattern2[i % 8];
-            answer3[i] = pattern3[i % 10];
         }
         
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
+        int maxScore = Math.max(Math.max(score1, score2), score3);
+        List<Integer> answerList = new ArrayList<>();
         
-        for(int j = 0; j < answerLength; j++){
-            if(answers[j] == answer1[j]){
-                count1++;
-            }
-            if(answers[j] == answer2[j]){
-                count2++;
-            }
-            if(answers[j] == answer3[j]){
-                count3++;
-            }
+        if(score1 == maxScore) answerList.add(1);
+        if(score2 == maxScore) answerList.add(2);
+        if(score3 == maxScore) answerList.add(3);
+        
+        int[] answer = new int[answerList.size()];
+        
+        for(int j = 0; j < answerList.size(); j++){
+            answer[j] = answerList.get(j);
         }
         
-        List<Integer> returnPeople = new ArrayList<>();
-        
-        int max = Math.max(Math.max(count1, count2), count3);
-        
-        if(max == count1){
-            returnPeople.add(1);
-        }
-        if(max == count2){
-            returnPeople.add(2);
-        }
-        if(max == count3){
-            returnPeople.add(3);
-        }
-        
-        int[] returnAnswer = new int[returnPeople.size()];
-        
-        for(int a = 0; a < returnPeople.size(); a++){
-            returnAnswer[a] = returnPeople.get(a);
-        }
-        
-        return returnAnswer;
+        return answer;
     }
 }
